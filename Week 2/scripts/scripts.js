e91abbf1ac25841a1956f6f3e72f9fa136a158a6
MckenzieMcCall1123 <msmccall@email.neit.edu>
2024-10-08 15:07:41 -0400

    all changes saved

---
+++ Week 2/scripts/scripts.js
@@ -0,0 +1,9 @@
+var newMessage = "John Cenas Mad."
+
+var messageTag = document.getElementById("message")
+
+function showMessageOnPage(){
+    messageTag.innerHTML = newMessage;
+}
+
+showMessageOnPage();
